b3c31aa17f768d7ae9da1969663d7698ce970536
Joseph Wang <joseph.wang@langlive.com>
2021-09-14 19:28:35 +0800

    Support StateFlow/SharedFlow

--- app/src/main/java/fr/afaucogney/mobile/android/flipper/flipperandroidbackstack/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/fr/afaucogney/mobile/android/flipper/flipperandroidbackstack/ui/notifications/NotificationsViewModel.kt
@@ -3,6 +3,10 @@ package fr.afaucogney.mobile.android.flipper.flipperandroidbackstack.ui.notifica
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class NotificationsViewModel : ViewModel() {
 
@@ -10,4 +14,15 @@ class NotificationsViewModel : ViewModel() {
         value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
+
+    private val _state = MutableStateFlow("state")
+    val state = _state.asStateFlow()
+
+    private val _shared = MutableSharedFlow<String>(5).apply {
+        repeat(10) {
+            tryEmit("shared:$it")
+        }
+    }
+    val shared = _shared.asSharedFlow()
+
 }
